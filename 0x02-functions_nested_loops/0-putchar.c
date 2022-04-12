163be6aa7ec321fa9bca0525fa64cc115590af4b
Girmangus Hailu <girmangus@milkta.com>
2022-04-12 22:56:26 +0300

    0x02 functions trial -1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,7 @@
  **/
 int main(void)
 {
-	char holberton[] = "abcdefghijklmnopqrstuvwxyz";
 
-	int c;
-
-	for (c = 0; c < 9; c++)
-	{
-		_putchar(holberton[c]);
-	}
 	_putchar('\n');
 	return (0);
 }
