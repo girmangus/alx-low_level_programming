d653fb6f8d6ce6bae2db24f7b22dd30efd247871
Girmangus Hailu <girmangus@milkta.com>
2022-04-19 01:29:13 +0300

    advance question 100 long error modification

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,32 @@
 #include "main.h"
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to convert
- *
- * Return: value of integer
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
  */
 int _atoi(char *s)
 {
-    int i, j, n, m;
+    short boolean;
+    int i, minus, result;
 
-    i = n = 0;
-    m = 1;
-    while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+    i = minus = result = boolean = 0;
+    minus = -1;
+
+    while (s[i] != '\0')
     {
-        if (*(s + i) == '-')
-            m *= -1;
+        if (s[i] == '-')
+            minus *= -1;
+
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result *= 10;
+            result -= (s[i] - '0');
+            boolean = 1;
+        }
+        else if (boolean == 1)
+        break;
         i++;
     }
-    j = i;
-    while ((*(s + j) >= '0') && (*(s + j) <= '9'))
-    {
-        n = n * 10 + m * (*(s + j) - '0');
-        j++;
-    }
-    return (n);
+    result *= minus;
+    return (result);
 }
