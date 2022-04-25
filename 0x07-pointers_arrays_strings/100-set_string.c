60da5b4e342fec11f7faff048639f2b6a37b8ae0
Girmangus Hailu <girmangus@milkta.com>
2022-04-25 22:49:00 +0300

    0x07. C - Even more pointers, arrays and strings advance 100 and 101 question

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
+
 /**
  * set_string - sets the value of a pointer to a char
- * @s: double pointer
- * @to: pointer
- *
+ * @s: double pointer to set
+ * @to: the char to point to.
  */
 void set_string(char **s, char *to)
 {
-	*s = to;
+    *s = to;
 }
