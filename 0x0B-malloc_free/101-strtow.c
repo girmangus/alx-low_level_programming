47be05784eb6d38ac97195b437eb41203a836fbd
Girmangus Hailu <girmangus@milkta.com>
2022-05-05 02:11:59 +0300

    0x0B-malloc_free advance question

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -47,14 +47,9 @@ char **strtow(char *str)
 {
 	int total_words = 0, b = 0, c = 0, length = 0;
 	char **words, *found_word;
-
-	if (str == 0 || *str == 0)
-		return (NULL);
 	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
 	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
+	if (str == 0 || *str == 0 || total_words == 0 || words == 0)
 		return (NULL);
 	for (; *str != '\0' &&  b < total_words;)
 	{
@@ -81,7 +76,9 @@ char **strtow(char *str)
 				c++;
 			}
 			words[b][c] = '\0';
-			b++; c = 0; length = 0;
+			b++; 
+			c = 0; 
+			length = 0;
 			str++;
 		}
 	}
