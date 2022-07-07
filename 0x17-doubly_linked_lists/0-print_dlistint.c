3da92949aa9626232148b41714e2f541a07a4228
Girmangus Hailu <girmangus@milkta.com>
2022-07-07 16:50:00 +0300

    0x17. C - Doubly linked lists  modification question 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,17 +3,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t con = 0;
+	size_t i;
 
-	if (h == NULL)
-		return (0);
-
-	while (h)
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
-		con++;
 		h = h->next;
 	}
-
-	return (con);
+	return (i);
 }
